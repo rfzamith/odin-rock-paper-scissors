5c7a623bced6ce9bf976038caeaf13ba12d86b0c
Rui <rzamith@gmail.com>
2022-05-26 21:39:32 +0100

    Finalised version

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ The computer will also randomly select an object, and the two hands will be comp
 
 // Function to write text before game starts
 let i = 0;
-let txt = 'Rock Paper Scissors';
+let txt = 'Rock, Paper, Scissors';
 let speed = 50;
 
 function typeWriter() {
@@ -35,35 +35,121 @@ function computerPlay(myArray) {
 }
 
 const objects = ['rock', 'paper', 'scissors'];
+let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
-function playGame(x,y) {
-  y = computerPlay(objects);
-  x = prompt('Choose your object!');
-  x = x.toLowerCase();
-  if(x == 'rock') { 
-    if(y == 'rock') { console.log('Rock vs Rock! It\'s a tie!'); }
-    if(y == 'paper') { console.log('Rock vs Paper! You lost!'); computerScore++; }
-    if(y == 'scissors') { console.log('Rock vs Scissors! You won!'); playerScore++; }
-  }
-  else if(x == 'paper') {
-    if(y == 'paper') { console.log('Paper vs Paper! It\'s a tie!'); }
-    if(y == 'scissors') { console.log('Paper vs Scissors! You lost!'); computerScore++ }
-    if(y == 'rock') { console.log('Paper vs Rock! You won!'); playerScore++; }
-  }
-  else if(x == 'scissors') {
-    if(y == 'scissors') { console.log('Scissors vs Scissors! It\'s a tie!'); }
-    if(y == 'rock') { console.log('Scissors vs Rock! You lost!'); computerScore++ }
-    if(y == 'paper') { console.log('Scissors vs Paper! You won!'); playerScore++; }
-  }
-  else { console.log('Object not found!'); }
-  console.log('Score: ' + playerScore + " x " + computerScore);
+const playerRock = document.querySelector('.rock');
+const playerPaper = document.querySelector('.paper');
+const playerScissors = document.querySelector('.scissors');
+const computerRock = document.querySelector('.rock2');
+const computerPaper = document.querySelector('.paper2');
+const computerScissors = document.querySelector('.scissors2');
+const playerSco = document.querySelector('.playerScore');
+const computerSco = document.querySelector('.computerScore');
+
+// playRound();
+
+// function playRound() {
+//   for (let i = 0; i < 5; i++) {
+//     playerRock.addEventListener('click', pickObject);
+//   }
+// }
+
+playerRock.addEventListener('click', pickRock);
+playerPaper.addEventListener('click', pickPaper);
+playerScissors.addEventListener('click', pickScissors);
+
+function pickRock(e) {
+  playGame('rock',computerPlay(objects));
+}
+function pickPaper(e) {
+  playGame('paper',computerPlay(objects));
+}
+function pickScissors(e) {
+  playGame('scissors',computerPlay(objects));
 }
 
-// for (let i = 0; i < 5; i++) {
-//   playGame();
-// }
+function clearAllObjects() {
+  playerRock.classList.value = 'rock';
+  playerPaper.classList.value = 'paper';
+  playerScissors.classList.value = 'scissors';
+  computerRock.classList.value = 'rock2';
+  computerPaper.classList.value = 'paper2';
+  computerScissors.classList.value = 'scissors2';
+}
+
+function myTimeout() {
+  t = setTimeout(clearAllObjects, 1000);
+}
+function stopTimeout() {
+  clearTimeout(t);
+}
+
+function playGame(x,y) {
+  clearAllObjects();
+  if(x == 'rock') {
+    playerRock.classList.value = 'rock-selected';
+    myTimeout();
+    if(y == 'rock') {
+      console.log('Rock vs Rock! It\'s a tie!');
+      computerRock.classList.value = 'rock-selected2';
+    }
+    if(y == 'paper') {
+      console.log('Rock vs Paper! You lost!');
+      computerPaper.classList.value = 'paper-selected2';
+      computerScore++;
+      computerSco.innerHTML = 'Score: ' + computerScore;
+    }
+    if(y == 'scissors') { 
+      console.log('Rock vs Scissors! You won!');
+      computerScissors.classList.value = 'scissors-selected2';
+      playerScore++; 
+      playerSco.innerHTML = 'Score: ' + playerScore;
+    }
+  }
+  else if(x == 'paper') {
+    playerPaper.classList.replace('paper','paper-selected');
+    myTimeout();
+    if(y == 'paper') { 
+      console.log('Paper vs Paper! It\'s a tie!');
+      computerPaper.classList.value = 'paper-selected2';
+    }
+    if(y == 'scissors') { 
+      console.log('Paper vs Scissors! You lost!');
+      computerScissors.classList.value = 'scissors-selected2';
+      computerScore++;
+      computerSco.innerHTML = 'Score: ' + computerScore;
+     }
+    if(y == 'rock') { 
+      console.log('Paper vs Rock! You won!');
+      computerRock.classList.value = 'rock-selected2';
+      playerScore++;
+      playerSco.innerHTML = 'Score: ' + playerScore;
+     }
+  }
+  else if(x == 'scissors') {
+    playerScissors.classList.replace('scissors','scissors-selected');
+    myTimeout();
+    if(y == 'scissors') { 
+      console.log('Scissors vs Scissors! It\'s a tie!'); 
+      computerScissors.classList.value = 'scissors-selected2';
+    }
+    if(y == 'rock') { 
+      console.log('Scissors vs Rock! You lost!'); 
+      computerRock.classList.value = 'rock-selected';
+      computerScore++;
+      computerSco.innerHTML = 'Score: ' + computerScore;
+    }
+    if(y == 'paper') { 
+      console.log('Scissors vs Paper! You won!'); 
+      computerPaper.classList.value = 'paper-selected2';
+      playerScore++; 
+      playerSco.innerHTML = 'Score: ' + playerScore;
+    }
+  }
+  console.log('Score: ' + playerScore + " x " + computerScore);
+}
 
 
 
